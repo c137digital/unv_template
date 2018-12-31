1158818c1840727c25f506762883b7ffd97523dc
Morty Space <morty.space@gmail.com>
2018-12-31 21:19:40 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='package',
+    version='0.1',
+    description="""Provide description""",
+    url='http://github.com/',
+    author='',
+    author_email='',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=['package'],
+    install_requires=[
+    ],
+    zip_safe=True
+)
---
+++ setup.sh
@@ -0,0 +1,5 @@
+python3 -m venv ./venv
+. activate.sh
+
+pip install --upgrade pip
+pip install -r requirements.txt
