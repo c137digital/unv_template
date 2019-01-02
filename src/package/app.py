29e224ea4c5625bf352949f0487a3c4821c68dbd
Morty Space <morty.space@gmail.com>
2019-01-02 21:37:11 +0200

    Added simple docs, added badge for build

--- src/package/app.py
+++ src/package/app.py
@@ -1,4 +1,6 @@
 class SomeExampleApp:
+    """This is example of documented class. Just powers two numbers
+    and increments call count."""
     def __init__(self, name: str = 'app'):
         self.name = name
         self.ncalls = 0
