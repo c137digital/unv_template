37a5ca24a6512b390ea549dd88a117343ed422dc
Morty Space <morty.space@gmail.com>
2019-01-02 21:48:13 +0200

    Added other python build for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import pathlib
-import sys
-sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent / 'src'))
-
 
 # -- Project information -----------------------------------------------------
 
